6b58efbc393d6078ffd08b88f9a2a6aeed4052a8
ilolivie <54978914+ilolivie@users.noreply.github.com>
2019-11-08 22:12:21 +0300

    основное задание

--- client.py
+++ client.py
@@ -1,23 +1,29 @@
 import asyncio
 
-
-HOST = 'localhost'
-PORT = 9095
-
-
-async def tcp_echo_client(host, port):
-    reader, writer = await asyncio.open_connection(host, port)
-    message = 'Hello, world'
-
-    writer.write(message.encode())
+async def tcp_echo_client(msg, loop):
+    reader, writer = await asyncio.open_connection('127.0.0.1', port, loop = loop)
+    print(f'Отправил сообщение: {msg}')
+    writer.write(msg.encode())
     await writer.drain()
 
     data = await reader.read(100)
+    print(f'Получил: {data.decode()}')
     writer.close()
-    # await writer.wait_closed()
-
-# asyncio.run(tcp_echo_client(HOST, PORT))
-
+    
+try:
+    port= input("Введите номер порта: ")
+    if port == '':
+        port = 9090
+        port = int(port)
+    if type(port) == int and 0 <= port <= 65535:
+        pass
+    else:
+        port = 9090
+except ValueError:
+    port = 9090
+    print("Введен некорректный порт. По умолчанию - 9090.")
+        
+msg = "Привет!"
 loop = asyncio.get_event_loop()
-task = loop.create_task(tcp_echo_client(HOST, PORT))
-loop.run_until_complete(task)
+loop.run_until_complete(tcp_echo_client(msg, loop))
+loop.close()
--- server.py
+++ server.py
@@ -1,21 +1,34 @@
 import asyncio
 
-HOST = 'localhost'
-PORT = 9095
-
 
 async def handle_echo(reader, writer):
     data = await reader.read(100)
-    message = data.decode()
-
+    msg = data.decode()
+    addr = writer.get_extra_info('peername')
+    print(f'Получил {msg} от {addr}')
+    
     writer.write(data)
     await writer.drain()
 
     writer.close()
 
 
+try:
+    port= input("Введите номер порта: ")
+    if port == '':
+        port = 9090
+        port = int(port)
+    if type(port) == int and 0 <= port <= 65535:
+        pass
+    else:
+        port = 9090
+except ValueError:
+    port = 9090
+    print("Введен некорректный порт. По умолчанию - 9090.")
+
+
 loop = asyncio.get_event_loop()
-coro = asyncio.start_server(handle_echo, HOST, PORT, loop=loop)
+coro = asyncio.start_server(handle_echo, '127.0.0.1', port, loop=loop)
 server = loop.run_until_complete(coro)
 
 # Serve requests until Ctrl+C is pressed
